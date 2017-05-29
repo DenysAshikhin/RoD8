530306d9921dfba54d287aa265f1c8394f400c63
tls <tls@10.242.105.88>
2017-05-29 08:26:41 -0400

    Moving the icon update

--- RoD8/src/DesktopLauncher.java
+++ RoD8/src/DesktopLauncher.java
@@ -5,7 +5,6 @@ public class DesktopLauncher {
 	
 	public static void main (String[] args) {
 		
-		System.out.println("Hello");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
 	}
