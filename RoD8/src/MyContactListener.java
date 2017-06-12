f3aa2a03504faab3f856412d8841951117fb9a2c
KieranMoynihan <kieranamoynihan@gmail.com>
2017-06-12 08:22:29 -0400

    work plz

--- RoD8/src/MyContactListener.java
+++ RoD8/src/MyContactListener.java
@@ -57,6 +57,16 @@ public class MyContactListener implements ContactListener{
 			playerOnGround++;
 		}
 		
+		if(fa.getUserData().equals("mFoot")){
+			
+			
+		}
+		
+		if(fb.getUserData().equals("mFoot")){
+			
+			
+		}
+		
 		if(fa.getUserData() != null && fa.getUserData().equals("crystal")){
 			
 			bodiesToRemove.add(fa.getBody());
