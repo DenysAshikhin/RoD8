a1af9cfbe42a735ee7ea9d99e6677e8c470a1c69
Kieran Moynihan <kieranamoynihan@gmail.com>
2017-06-22 20:51:29 -0400

    gfda

--- RoD8/src/GameScreen.java
+++ RoD8/src/GameScreen.java
@@ -559,7 +559,7 @@ public class GameScreen implements Screen{
 		
 			if (teleporter.wasActivated){
 				
-				int tempTime = (int)((1*1000) - (System.currentTimeMillis() - portalStart))/1000;
+				int tempTime = (int)((60*1000) - (System.currentTimeMillis() - portalStart))/1000;
 				GlyphLayout guiLayout = new GlyphLayout(scoreFont, "Time Remaining: " + tempTime);
 			
 				if(tempTime > 0){
